22fafbd8dfa31fa1685e3195b976d4825dd19f70
Arshad <arafatarshad@gmail.com>
2019-04-22 10:08:26 -0300

    project entry point front end doe

--- apps/project_ground/admin.py
+++ apps/project_ground/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import DatasetType
+from .models import DatasetType,Project
 # Register your models here.
 
+class ProjectAdmin(admin.ModelAdmin):
+    list_display=('id','author_first_name','author_last_name','description','email','project_reference','dataset_type_id','basefilename','dataset')
 
-admin.site.register(DatasetType)
+class DatasetTypeAdmin(admin.ModelAdmin):
+    list_display=('id','name')
+
+
+admin.site.register(DatasetType,DatasetTypeAdmin)
+admin.site.register(Project,ProjectAdmin)
--- apps/project_ground/views.py
+++ apps/project_ground/views.py
@@ -1,8 +1,10 @@
+import os
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from .models import DatasetType
+from .models import Project
 from .forms import ProjectForm
 
 
@@ -22,9 +24,15 @@ class CreateProject(APIView):
         def post(self, request, *args, **kwargs):
             form = ProjectForm(request.POST,  request.FILES)
             if form.is_valid():
-                html = "<html><body>It is now %s.</body></html>"
-                return HttpResponse(html)
+                new_project=form.save(commit=False)
+                new_project.dataset_type_id_id=request.POST['dataset_type_id']
+                new_project.save()
+                print(new_project.dataset_type_id)
+
+                template_name = 'project_ground/project_entry.html'
+
+                return render(request, template_name, {new_project:new_project})
             else :
-                return render(request,"project_ground/temp.html",{"page_title":"Create New Project",
+                return render(request,"project_ground/create_project.html",{"page_title":"Create New Project",
                                                                         "dataset_types":DatasetType.objects.all(),
                                                                         "form":form})
