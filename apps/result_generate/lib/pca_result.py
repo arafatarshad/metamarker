6c3125741d9f6472fb30721384bfbfe3c8a40eef
Arshad <aarafat@mun.ca>
2020-02-10 21:58:15 -0330

    PCA basic visualization done

--- apps/result_generate/lib/pca_result.py
+++ apps/result_generate/lib/pca_result.py
@@ -23,22 +23,18 @@ import plotly.figure_factory as ff
 
 
 class PCAResultLIB:
-    job=None
-    def __init__(self,job):
-        self.job=job
-        self.pca=PcaResult.objects.filter(job_id=self.job.id)[0]
-
-        # print( )
-
-        # for c in self.pca.variance_explained:
-            # print (c)
-        # print(self.pca.variance_explained)
-
+    pca=None
+    def __init__(self,pca):
+        self.pca=pca
     def getMeComponentsId(self):
         components=ComponentResult.objects.filter(pca_result_id=self.pca.id)
         components_id=[]
+        iterator=1
         for com in components:
-            components_id.append(com.id)  
+            name="PC_"+(str)(iterator)
+            components_id.append({'id':com.id,'name':name})
+            iterator=iterator+1
+        components_id
         return components_id
 
     def generateThePieChart(self):
@@ -50,105 +46,19 @@ class PCAResultLIB:
             values.append(pcas[key])
         figure = go.Figure(data=[go.Pie(labels=labels, values=values)])
         return  opy.plot(figure, auto_open=False, output_type='div')
-#
-# class Pca:
-#     df=None
-#     X=None
-#     Y=None
-#     features=None
-#     target=None
-#     job=None
-#
-#     def __init__(self,job):
-#         self.job=job
-#         self.df=self.getUsProperDf(job)._get_numeric_data()
-#         self.features=list(self.df.columns.values)
-#         self.target=self.features[-1]
-#         self.features.remove(self.target)
-#
-#         self.X=self.df
-#         self.Y=self.df[self.target]
-#         self.X.drop(self.target,axis=1, inplace=False)
-#
-#         self.X= self.X.fillna(self.X.mean())
-#         self.StandardScale()
-#         # self.Y= self.X.fillna(self.X.mean())
-#
-#     def getUsProperDf(self,job):
-#         project = Project.objects.get(id=job.project.id)
-#         if job.extradataset_id ==None:
-#             return pd.read_csv(project.dataset)
-#         else:
-#             filename=job.extradataset.basefilename
-#             return pd.read_csv("uploads/"+filename)
-#
-#
-#     def StandardScale(self):
-#         # print("we are currently here ---------------->")
-#         job_params= PcaJobParameters.objects.filter(job_id=self.job.id)[0]
-#         print(job_params)
-#
-#         if job_params.standard_scale==1:
-#             self.X = StandardScaler().fit_transform(self.X)
-#
-#         pca = PCA(n_components=job_params.no_of_components).fit(self.X)
-#         pca.transform(self.X)
-#         n_pcs= pca.components_.shape[0]
-#
-#         self.updateDB(pca,job_params)
-#
-#
-#     def updateDB(self,pca,job_params):
-#
-#         variance_ratio={}
-#         component_values=pca.components_[0]
-#         n_target_features=math.floor((job_params.reduce_to/100.00)*len(self.features))
-#         counter = 1
-#
-#         for i in pca.explained_variance_ratio_:
-#             variance_ratio["PC_"+str(counter)] = i
-#             counter=counter+1
-#
-#         pca_result=PcaResult(job_id=self.job.id,variance_explained=json.dumps(variance_ratio))
-#         pca_result.save()
-#
-#
-#         for i in range(job_params.no_of_components):
-#             feature_list={}
-#             for j in range(n_target_features):
-#                 location=np.abs(component_values).argmax()
-#                 value=pca.components_[0][location]
-#                 component_values[location]=0
-#                 feature_list[self.features[location-1]]=value
-#             ComponentResult(component_id=i,result=json.dumps(feature_list),pca_result_id=pca_result.id).save()
-#
-#         # print("-------------pca applied success-----------")
-#         self.job.status=2
-#         self.job.save()
-#
 
 
 
 
+class ComponentsResultLib:
 
-
-        # x_new = pca.fit_transform(self.X)
-
-        # # n=len(self.X)
-        # # self.myplot(x_new[:,0:n],np.transpose(pca.components_[0:n, :]))
-        # print(x_new[:,0:len(self.X)])
-        # print(40*'-')
-        # print(pca.components_[0:len(self.X), :][0])
-        # print(x_new[0,0:3])
-        # print(self.X)
-
-        # most_important = [np.abs(pca.components_[i]).argmax() for i in range(n_pcs)]
-        # most_important_names = [self.features[most_important[i]] for i in range(n_pcs)]
-        # print(most_important)
-        # dic = {'PC{}'.format(i): most_important_names[i] for i in range(n_pcs)}
-        # #
-        # # # build the dataframe
-        # newdf = pd.DataFrame(dic.items())
-        # print(pca.components_[0])
-
-        # print(pca.components_[0])
+    def returnBarChart(self,id):
+        com_result=json.loads(ComponentResult.objects.filter(id=id)[0].result)
+        labels=[]
+        values=[]
+        # print(com_result)
+        for key in com_result:
+            labels.append(key)
+            values.append(com_result[key])
+        figure = go.Figure(data=[go.Bar(x=labels, y=values)])
+        return  opy.plot(figure, auto_open=False, output_type='div')
--- apps/result_generate/views.py
+++ apps/result_generate/views.py
@@ -15,7 +15,7 @@ import json
 from django.core import serializers
 
 from background_task import background
-from .lib.pca_result import PCAResultLIB
+from .lib.pca_result import PCAResultLIB,ComponentsResultLib
 
 from django.http import JsonResponse
 import json
@@ -24,10 +24,15 @@ import json
 class JobResult(APIView):
         def showJobReult(request,id):
             job = Job.objects.get(pk=id)
+
             if job.processing_algorithm.reference_id == 'pc_001':
-                pca_result =PCAResultLIB(job)
+                pca_result =PCAResultLIB(PcaResult.objects.filter(job_id=id)[0])
                 pie_chart=pca_result.generateThePieChart()
                 pc_id=pca_result.getMeComponentsId()
             project=Project.objects.filter(reference_id=request.session['reference_id']).first()
-
             return render(request,"show_result/pca_result.html",{"page_title":"PCA-Result","project_id":project.id,"pie_chart":pie_chart,"pc_id":pc_id})
+
+        def getPCAComponentResult(request,id):
+            figure=ComponentsResultLib().returnBarChart(id)
+            # return JsonResponse(figure,safe=False)
+            return HttpResponse(figure)
