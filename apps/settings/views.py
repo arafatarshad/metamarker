46e6a758433d89a68c43f890e3cc3e19744cc928
Arshad <aarafat@mun.ca>
2020-03-03 11:07:00 -0330

    settings added

--- apps/settings/views.py
+++ apps/settings/views.py
@@ -13,5 +13,20 @@ class Settings(APIView):
             project = Project.objects.get(reference_id=request.session['reference_id'])
             # dataset=ExtraDataset.objects.filter(project_id=project)
             # preprocessing_tasks=PreprocessingTasks.objects.all()
-            # return render(request,"settings/settings.html",{"page_title":"settings"})
+            return render(request,"settings/settings.html",{"page_title":"settings","project":project})
+            # return HttpResponse(project)
+        def post(self, request, *args, **kwargs):
+            project = Project.objects.get(reference_id=request.session['reference_id'])
+            project.description=request.POST['description']
+            project.author_first_name=request.POST['author_first_name']
+            project.author_last_name=request.POST['author_last_name']
+            # project.email=request.POST['email']
+            project.save()
+            # print(request.POST)
+            # dataset=ExtraDataset.objects.filter(project_id=project)
+            # preprocessing_tasks=PreprocessingTasks.objects.all()
+            # return HttpResponse(project)
+            return render(request,"settings/settings.html",{"page_title":"settings","project":project})
+        def deleteProject(self,id):
+            project = Project.objects.get(id=id)
             return HttpResponse(project)
